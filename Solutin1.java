43f45de5c9d4dc466036cde31e336d077f5c148e
nur6ek <138194910+nur6ek@users.noreply.github.com>
2023-06-30 14:08:33 +0500

    End of lessons

---
+++ Solutin1.java
@@ -0,0 +1,21 @@
+https://acmp.ru/asp/do/index.asp?main=task&id_course=1&id_section=1&id_topic=28&id_problem=160
+
+import java.util.Scanner;
+class EndOfLesson {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(9 + (n * 45 + (n / 2) * 5 + (n - 1) / 2 * 15) / 60 + " " + (n * 45 + (n / 2) * 5 + (n - 1) / 2 * 15) % 60);
+    }
+}
+
+
+/* C++ solution
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    cout<<9+(n*45+(n/2)*5+(n-1)/2*15)/60<<' '<<(n*45+(n/2)*5+(n-1)/2*15)%60;
+    return 0;
+}
