339a446a64e8d6374e027366313f92bbb070a410
nur6ek <138194910+nur6ek@users.noreply.github.com>
2023-06-30 14:11:18 +0500

    Update Solutin.java

--- Solutin1.java
+++ Solutin1.java
@@ -1,4 +1,4 @@
-https://acmp.ru/asp/do/index.asp?main=task&id_course=1&id_section=1&id_topic=28&id_problem=160
+// Problem link https://acmp.ru/asp/do/index.asp?main=task&id_course=1&id_section=1&id_topic=28&id_problem=160
 
 import java.util.Scanner;
 class EndOfLesson {
@@ -19,3 +19,4 @@ int main(){
     cout<<9+(n*45+(n/2)*5+(n-1)/2*15)/60<<' '<<(n*45+(n/2)*5+(n-1)/2*15)%60;
     return 0;
 }
+*/
